584bbfee8b78c49e20dfefe7626baecd6d918e48
Yugantar <yugantar8jain@gmail.com>
2021-04-18 23:04:09 +0530

    workout scene instruction added.

--- Sources/B Workout/Views/WorkoutScene.swift
+++ Sources/B Workout/Views/WorkoutScene.swift
@@ -27,14 +27,21 @@ public struct WorkoutScene: View {
                 .offset(y: workoutModel.pushupState == .up ? 160 : 170)
             
             HStack {
-                Spacer()
-                
-                Text("Reps: \(workoutModel.reps)")
+                if workoutModel.reps == 0 {
+                    switch workoutModel.pushupState {
+                    case .up: Label("Use the Down arrow key to go down", systemImage: "arrowtriangle.down.square.fill")
+                    case .down: Label("Use the Up arrow key to push back up", systemImage: "arrowtriangle.up.square.fill")
+                    }
+                } else {
+                    Spacer()
+
+                    Text("Reps: \(workoutModel.reps)")
+                }
             }
-            .foregroundColor(.white)
             .font(.system(size: 32, weight: .black, design: .monospaced))
+            .foregroundColor(.white)
             .padding(40)
-            .offset(y: -50)
+            .offset(y: -80)
             .frame(width: Layout.width)
             
             BackButton()
